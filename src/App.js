9adee277abd581c9f29aa4c936b73e19075aaaf5
Paul Landaeta <paul.landeata@coxautoinc.com>
2022-08-31 07:58:51 -0400

    fix problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import React, { useState } from 'react';
-import { PokeCard } from './components/PokeCard';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { PokeCard } from "./components/PokeCard";
+import API from "./config/api";
 
 function App() {
-  return (
-    <PokeCard></PokeCard>
-  );
+  const [trainer, setTrainer] = useState();
+  const getTrainerId = async () => {
+    const trainerReponse = await API.get(`/trainer/3`);
+    const { data } = trainerReponse;
+    setTrainer(data.trainer[0]);
+    console.log(data.trainer);
+  };
+  useEffect(() => {
+    getTrainerId();
+  }, []);
+
+  return <>{trainer && <PokeCard idPokemon={trainer.id_pokemon}></PokeCard>}</>;
 }
 
 export default App;
